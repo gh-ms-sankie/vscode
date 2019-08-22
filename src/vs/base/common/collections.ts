a12f55f6f5bd165e859b02f698b45095d0e7e9e1
rahulku <rahulskumar@gmail.com>
2019-08-22 11:28:23 -0700

    Update collections.ts

--- src/vs/base/common/collections.ts
+++ src/vs/base/common/collections.ts
@@ -1,3 +1,6 @@
+Test
+Test
+Test
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
